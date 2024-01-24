aa267c6c58f57b5851e9f8529681dc9be57ea62e
sanchick02 <115912003+sanchick02@users.noreply.github.com>
2024-01-24 17:15:56 +0800

    fixed the login and sign up

--- lib/providers/user_provider.dart
+++ lib/providers/user_provider.dart
@@ -3,7 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:moodee/models/user_model.dart';
 
-User? user = FirebaseAuth.instance.currentUser!;
+User? get user => FirebaseAuth.instance.currentUser!;
 
 class UserProvider with ChangeNotifier {
   UserModel? userProviderData = UserModel(
