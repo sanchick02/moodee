96fa7946e829d57fb507c11b39714a1a321a781f
Sanjiv <115912003+sanchick02@users.noreply.github.com>
2024-01-25 17:13:28 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,20 @@
-import 'package:firebase_core/firebase_core.dart';
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
-import 'package:moodee/screens/animation_screen.dart';
-import 'package:moodee/screens/community/audio_room.dart';
-import 'package:moodee/screens/home_screen.dart';
-import 'package:moodee/screens/splash_screen.dart';
+import 'package:moodee/auth_widget_tree.dart';
+import 'package:moodee/providers/user_provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+import 'package:provider/provider.dart';
+import 'firebase_options.dart';
+
+List<CameraDescription>? cameras = [];
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  cameras = await availableCameras();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -16,9 +23,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: AnimationScreen(),
-      debugShowCheckedModeBanner: false,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(
+          value: UserProvider(),
+        ),
+      ],
+      child: const MaterialApp(
+        home: AuthWidgetTree(),
+      ),
     );
   }
 }
