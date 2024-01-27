ca2317b3d4a01d9fbb33f7ff45af4304343eb8b8
ehznauhcmil <135931394+ehznauhcmil@users.noreply.github.com>
2024-01-27 19:59:36 +0800

    therapist

--- lib/providers/user_provider.dart
+++ lib/providers/user_provider.dart
@@ -3,7 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:moodee/models/user_model.dart';
 
-final user = FirebaseAuth.instance.currentUser!;
+User? get user => FirebaseAuth.instance.currentUser!;
 
 class UserProvider with ChangeNotifier {
   UserModel? userProviderData = UserModel(
@@ -16,23 +16,33 @@ class UserProvider with ChangeNotifier {
   );
 
   Future<void> fetchUserData() async {
-    await FirebaseFirestore.instance
-        .collection('users')
-        .doc(user.uid)
-        .get()
-        .then(
-      (snapshot) {
-        userProviderData!.uid = snapshot.id;
-        userProviderData!.firstName = snapshot.data()!['first_name'];
-        userProviderData!.lastName = snapshot.data()!['last_name'];
-        userProviderData!.email = snapshot.data()!['email'];
-        userProviderData!.gender = snapshot.data()!['gender'];
+    if (user != null) {
+      try {
+        await FirebaseFirestore.instance
+            .collection('users')
+            .doc(user!.uid)
+            .get()
+            .then(
+          (snapshot) {
+            userProviderData!.uid = snapshot.id;
+            userProviderData!.firstName = snapshot.data()!['first_name'];
+            userProviderData!.lastName = snapshot.data()!['last_name'];
+            userProviderData!.email = snapshot.data()!['email'];
+            userProviderData!.gender = snapshot.data()!['gender'];
 
-        print('User data fetched');
+            print('User data fetched');
 
-        print(userProviderData!.uid);
-        print(userProviderData!.firstName);
-      },
-    );
+            print(userProviderData!.uid);
+            print(userProviderData!.firstName);
+            print(userProviderData!.lastName);
+            print(userProviderData!.email);
+          },
+        );
+      } catch (e) {
+        print(e);
+      }
+    } else {
+      print('User is null');
+    }
   }
 }
