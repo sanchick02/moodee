12e7d32cbb016038ea811654823f0bd45ad56c24
sanchick02 <115912003+sanchick02@users.noreply.github.com>
2024-01-28 01:48:16 +0800

    for claudia to edit

--- lib/data/events.dart
+++ lib/data/events.dart
@@ -2,75 +2,163 @@ import 'package:moodee/models/events_model.dart';
 
 List<Event> eventList = [
   Event(
-    name: "Art & Expressive Therapy",
-    image: "lib/assets/images/event1.png",
-    description: "Emotional release",
-    location: "Arts Centre KL",
-    month: "Jan",
-    date: "07",
-    day: "Sunday",
-    time: "2:30pm - 4:00pm",
-    eventHighlights: [
-      "• Unleash your emotions through the power of art, finding a creative and therapeutic outlet for self-expression.",
-      "• Connect with others in a supportive environment, understanding each other's journeys through the universal language of art.",
-      "• Experience the calming effects of art, guided by experts, regardless of your artistic background—no pressure, just pure creative exploration."
-    ],
-    eventImages: [
-      "lib/assets/images/event1.png",
-      "lib/assets/images/event1.png",
-      "lib/assets/images/event1.png",
-      "lib/assets/images/event1.png"
-    ],
-  ),
+      name: 'Serenity Yoga Session',
+      description: 'Stress Relief Yoga',
+      location: 'Yoga Studio KL',
+      date: '5',
+      day: 'Tuesday',
+      time: '6:00p.m.-7:30p.m.',
+      image: 'lib/assets/images/events/event01.png',
+      month: 'Dec',
+      eventHighlights: [
+        '• Join us for a rejuvenating yoga session designed to release stress and tension.',
+        '• Learn breathing techniques to promote relaxation and mental clarity.',
+        '• Suitable for all levels, beginners welcome!'
+      ],
+      eventImages: [
+        'lib/assets/images/events/event01img1.png',
+        'lib/assets/images/events/event01img2.png',
+        'lib/assets/images/events/event01img3.png',
+        'lib/assets/images/events/event01img4.png'
+      ]),
   Event(
-    name: "Mindfulness Workshop",
-    image: "lib/assets/images/event2.jpg",
-    description: "Stress Management",
-    location: "Sunway Pyramid",
-    month: "Jan",
-    date: "08",
-    day: "Monday",
-    time: "10:00am - 11:00am",
-    eventHighlights: ["• ", "• ", "• "],
-    eventImages: [
-      "lib/assets/images/event1.png",
-      "lib/assets/images/event1.png",
-      "lib/assets/images/event1.png",
-      "lib/assets/images/event1.png"
-    ],
-  ),
+      name: 'Art & Expressive Therapy',
+      description: 'Emotional release',
+      location: 'Arts Centre KL',
+      date: '7',
+      day: 'Sunday',
+      time: '2:30pm-4:00pm',
+      image: 'lib/assets/images/events/event02.png',
+      month: 'Jan',
+      eventHighlights: [
+        "• Unleash your emotions through the power of art, finding a creative and therapeutic outlet for self-expression.",
+        "• Connect with others in a supportive environment, understanding each other's journeys through the universal language of art.",
+        "• Experience the calming effects of art, guided by experts, regardless of your artistic background—no pressure, just pure creative exploration."
+      ],
+      eventImages: [
+        'lib/assets/images/events/event02img1.png',
+        'lib/assets/images/events/event02img2.png',
+        'lib/assets/images/events/event02img3.png',
+        'lib/assets/images/events/event02img4.png'
+      ]),
   Event(
-    name: "Book Clubs & Literature Circles",
-    image: "lib/assets/images/event3.jpg",
-    description: "Safe space for discussion",
-    location: "Book Xcess KL",
-    month: "Jan",
-    date: "18",
-    day: "Thursday",
-    time: "8:00pm - 10:00pm",
-    eventHighlights: ["• ", "• ", "• "],
-    eventImages: [
-      "lib/assets/images/event1.png",
-      "lib/assets/images/event1.png",
-      "lib/assets/images/event1.png",
-      "lib/assets/images/event1.png"
-    ],
-  ),
+      name: 'Book Clubs & Literature Circles',
+      description: 'Safe space for discussion',
+      location: 'Book Xcess KL',
+      date: '18',
+      day: 'Thursday',
+      time: '8:00pm-10:00pm',
+      image: 'lib/assets/images/events/event03.png',
+      month: 'Jan',
+      eventHighlights: [
+        '• Join us for a thought-provoking discussion on the book "The Mindful Path to Self-Compassion.',
+        '• Share your insights and experiences related to mental well-being.',
+        '• Connect with fellow book enthusiasts in a relaxed and supportive environment.'
+      ],
+      eventImages: [
+        'lib/assets/images/events/event03img1.png',
+        'lib/assets/images/events/event03img2.png',
+        'lib/assets/images/events/event03img3.png',
+        'lib/assets/images/events/event03img4.png'
+      ]),
   Event(
-    name: "Forest Bathing Excursions",
-    image: "lib/assets/images/event4.jpg",
-    description: "Immerse in nature",
-    location: "Bukit Gasing",
-    month: "Jan",
-    date: "24",
-    day: "Sunday",
-    time: "6:00am - 8:00am",
-    eventHighlights: ["• ", "• ", "• "],
-    eventImages: [
-      "lib/assets/images/event1.png",
-      "lib/assets/images/event1.png",
-      "lib/assets/images/event1.png",
-      "lib/assets/images/event1.png"
-    ],
-  ),
+      name: 'Nature Harmony Walk',
+      description: 'Mindful Nature Walk',
+      location: 'Botanical Garden',
+      date: '10',
+      day: 'Sunday',
+      time: '7:00a.m.-9:00a.m.',
+      image: 'lib/assets/images/events/event04.png',
+      month: 'Dec',
+      eventHighlights: [
+        '• Connect with nature and your inner self on this guided mindful walk.',
+        '• Experience the therapeutic benefits of grounding exercises.',
+        '• Enjoy the beauty of a local park while practicing mindfulness.'
+      ],
+      eventImages: [
+        'lib/assets/images/events/event04img1.png',
+        'lib/assets/images/events/event04img2.png',
+        'lib/assets/images/events/event04img3.png',
+        'lib/assets/images/events/event04img4.png'
+      ]),
+  Event(
+      name: 'Forest Bathing Excursions',
+      description: 'Immerse in nature',
+      location: 'Bukit Gasing',
+      date: '24',
+      day: 'Sunday',
+      time: '6:00am-8:00am',
+      image: 'lib/assets/images/events/event05.png',
+      month: 'Jan',
+      eventHighlights: [
+        '• Immerse yourself in the healing power of nature with a guided forest bathing experience.',
+        '• Engage in mindfulness activities surrounded by the lush greenery of Bukit Gasing.',
+        '• Experience the calming effects of nature on mental well-being.'
+      ],
+      eventImages: [
+        'lib/assets/images/events/event05img1.png',
+        'lib/assets/images/events/event05img2.png',
+        'lib/assets/images/events/event05img3.png',
+        'lib/assets/images/events/event05img4.png'
+      ]),
+  Event(
+      name: 'Harmony Sound Session',
+      description: 'Sound Healing Meditation',
+      location: 'Wellness Center',
+      date: '13',
+      day: 'Tuesday',
+      time: '7:00p.m.-8:30p.m.',
+      image: 'lib/assets/images/events/event06.png',
+      month: 'Jan',
+      eventHighlights: [
+        '• Experience the therapeutic effects of sound vibrations on the mind.',
+        '• Relax deeply as soothing sounds promote mental and emotional balance.',
+        '• Immerse yourself in a sound bath for a rejuvenating experience.'
+      ],
+      eventImages: [
+        'lib/assets/images/events/event06img1.png',
+        'lib/assets/images/events/event06img2.png',
+        'lib/assets/images/events/event06img3.png',
+        'lib/assets/images/events/event06img4.png'
+      ]),
+  Event(
+      name: 'Mindfulness Workshop',
+      description: 'Stress Management',
+      location: 'Sunway Pyramid',
+      date: '8',
+      day: 'Monday',
+      time: '8:00a.m.-1:00p.m.',
+      image: 'lib/assets/images/events/event07.png',
+      month: 'Jan',
+      eventHighlights: [
+        '• Embark on a journey of self-discovery through our Mindfulness Workshop.',
+        '• Cultivate awareness and presence in the present moment, guided by experienced facilitators.',
+        '• Explore mindfulness techniques to foster a calm and centered mind, enhancing your overall well-being.'
+      ],
+      eventImages: [
+        'lib/assets/images/events/event07img1.png',
+        'lib/assets/images/events/event07img2.png',
+        'lib/assets/images/events/event07img3.png',
+        'lib/assets/images/events/event07img4.png'
+      ]),
+  Event(
+      name: 'Nourish Your Soul',
+      description: 'Mindful Eating Workshop',
+      location: 'Health&Wellness Hub',
+      date: '18',
+      day: 'Sunday',
+      time: '11:00a.m.-1:00p.m.',
+      image: 'lib/assets/images/events/event08.png',
+      month: 'Jan',
+      eventHighlights: [
+        '• Learn to cultivate awareness and joy through mindful eating practices.',
+        '• Explore the connection between food, emotions, and well-being.',
+        '• Enjoy a mindful tasting experience and discover a new relationship with food.'
+      ],
+      eventImages: [
+        'lib/assets/images/events/event08img1.png',
+        'lib/assets/images/events/event08img2.png',
+        'lib/assets/images/events/event08img3.png',
+        'lib/assets/images/events/event08img4.png'
+      ])
 ];
--- lib/data/therapists.dart
+++ lib/data/therapists.dart
@@ -9,11 +9,11 @@ List<Therapist> therapistList = [
     rating: "4.9",
     imageCard: "lib/assets/images/therapist1bg.png",
     achievements: [
-      "• Published research contributing to mental health knowledge.",
-      "• ABCT Awards in cognitive-behavioral therapy research and practice.",
-      "• ABCT Awards in cognitive-behavioral therapy research and practice.",
-      "• ABCT Awards in cognitive-behavioral therapy research and practice.",
-      "• ABCT Awards in cognitive-behavioral therapy research and practice."
+      "• Graduated from Harvard University",
+      "• Published research on adolescent mental health",
+      "• Recognized for outstanding work in youth empowerment programs",
+      "• Certified in Cognitive Behavioral Therapy for adolescents",
+      "• Conducted workshops on mindfulness and stress reduction"
     ],
     availability: [
       TherapistAvailability(
@@ -45,11 +45,11 @@ List<Therapist> therapistList = [
     rating: "4.8",
     imageCard: "lib/assets/images/therapist2bg.png",
     achievements: [
-      "• Published research contributing to mental health knowledge.",
-      "• ABCT Awards in cognitive-behavioral therapy research and practice.",
-      "• ABCT Awards in cognitive-behavioral therapy research and practice.",
-      "• ABCT Awards in cognitive-behavioral therapy research and practice.",
-      "• ABCT Awards in cognitive-behavioral therapy research and practice."
+      "• Ph.D. in Clinical Psychology from Stanford University",
+      "• Specialized in trauma-informed care",
+      "• Established a successful mental health podcast",
+      "• Contributed articles to leading psychology journals",
+      "• Founded a nonprofit for mental health awareness"
     ],
     availability: [
       TherapistAvailability(
@@ -81,9 +81,11 @@ List<Therapist> therapistList = [
     rating: "4.8",
     imageCard: "lib/assets/images/therapist3bg.png",
     achievements: [
-      "• ABCT Awards in cognitive-behavioral therapy research and practice.",
-      "• ABCT Awards in cognitive-behavioral therapy research and practice.",
-      "• ABCT Awards in cognitive-behavioral therapy research and practice."
+      "• Board-certified music therapist",
+      "• Developed music-based interventions for anxiety",
+      "• Collaborated with renowned British musicians on therapeutic projects",
+      "• Presented at international conferences on music therapy",
+      "• Featured in BBC for mental health advocacy"
     ],
     availability: [
       TherapistAvailability(
@@ -115,11 +117,11 @@ List<Therapist> therapistList = [
     rating: "4.6",
     imageCard: "lib/assets/images/therapist4bg.png",
     achievements: [
-      "• Published research contributing to mental health knowledge.",
-      "• ABCT Awards in cognitive-behavioral therapy research and practice.",
-      "• ABCT Awards in cognitive-behavioral therapy research and practice.",
-      "• ABCT Awards in cognitive-behavioral therapy research and practice.",
-      "• ABCT Awards in cognitive-behavioral therapy research and practice."
+      "• M.S. in Youth Counseling from NYU",
+      "• Established a mentorship program for at-risk youth",
+      "• Expertise in positive psychology interventions",
+      "• Presented at conferences on diversity in mental health",
+      "• Published research on the intersection of culture and mental health"
     ],
     availability: [
       TherapistAvailability(
