aa62515c51f3be16bb17f29b5711e54914b59760
sanchick02 <sanchick02@users.noreply.github.com>
2024-05-05 16:46:44 +0800

    fixed the bug where the mood tracker was not updating the mood correctly

--- lib/providers/mood_tracker_provider.dart
+++ lib/providers/mood_tracker_provider.dart
@@ -4,64 +4,70 @@ import 'package:flutter/material.dart';
 import 'package:moodee/models/mood_tracker.dart';
 
 class MoodTrackerProvider with ChangeNotifier {
+  final morningStart = DateTime(
+      DateTime.now().year, DateTime.now().month, DateTime.now().day, 0);
+  final morningEnd = DateTime(
+      DateTime.now().year, DateTime.now().month, DateTime.now().day, 12);
 
-  final morningStart = DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day, 0); 
-  final morningEnd = DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day, 12);
+  final afternoonStart = DateTime(
+      DateTime.now().year, DateTime.now().month, DateTime.now().day, 12);
+  final afteroonEnd = DateTime(
+      DateTime.now().year, DateTime.now().month, DateTime.now().day, 17);
 
-  final afternoonStart = DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day, 12); 
-  final afteroonEnd = DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day, 17);
+  final eveningStart = DateTime(DateTime.now().year, DateTime.now().month,
+      DateTime.now().day, 17); // 6 AM
+  final eveningEnd = DateTime(
+      DateTime.now().year, DateTime.now().month, DateTime.now().day, 21);
 
-  final eveningStart = DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day, 17); // 6 AM
-  final eveningEnd = DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day, 21);
-
-  final nightStart = DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day, 21); // 6 AM
-  final nightEnd = DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day + 1, 6);
+  final nightStart = DateTime(DateTime.now().year, DateTime.now().month,
+      DateTime.now().day, 21); // 6 AM
+  final nightEnd = DateTime(
+      DateTime.now().year, DateTime.now().month, DateTime.now().day + 1, 6);
 
   List<MoodTracker> moodData = [];
 
-  Future<List<MoodTracker>> fetchMoodData() async {
-    FirebaseAuth auth = FirebaseAuth.instance;
-    User? user = auth.currentUser;
-    if (user != null) {
-      try {
-        // Get the reference to your collection
+  Future<void> fetchMoodData() async {
+    try {
+      FirebaseAuth auth = FirebaseAuth.instance;
+      User? user = auth.currentUser;
+      if (user != null) {
+        // Get the reference to the mood collection
         CollectionReference moodCollectionReference =
-        FirebaseFirestore.instance.collection('moods');
+            FirebaseFirestore.instance.collection('moods');
 
         DocumentReference userMoodDoc = moodCollectionReference.doc(user.uid);
 
-        CollectionReference userMoodCollection = userMoodDoc.collection('user_moods');
+        CollectionReference userMoodCollection =
+            userMoodDoc.collection('user_moods');
 
         // Query the documents within the collection
         QuerySnapshot userMoodsSnapshot = await userMoodCollection.get();
 
+        moodData.clear(); // Empty the list for fresh data
+
         // Extract data from the documents
-        if (moodData.isEmpty) {
-        List<MoodTracker> fetchedMoodData = userMoodsSnapshot.docs.map((doc) {
-          return MoodTracker(
+        for (var doc in userMoodsSnapshot.docs) {
+          MoodTracker moodTracker = MoodTracker(
             image: doc['image'],
-            type: doc['type'], 
-            date: doc['date'], 
-            moodIntensity: doc['moodIntensity'], 
-            timePeriod: doc['timePeriod'], 
+            type: doc['type'],
+            date: doc['date'],
+            moodIntensity: doc['moodIntensity'],
+            timePeriod: doc['timePeriod'],
             userId: doc['userId'],
             question: doc['question'],
             answer: doc['answer'],
             moodTrackerStreak: doc['moodTrackerStreak'],
-            );
-        }).toList();
-  
-  // Update the list of mood data
-        moodData = fetchedMoodData;
-      }
+          );
+          moodData.add(moodTracker);
+        }
 
-        // Notify listeners about the change in data
-        notifyListeners();
-      } catch (e) {
-        print(e.toString());
+        print('All mood data fetched');
+        print(moodData[0].type);
+        print(moodData[0].image);
+        notifyListeners(); // Notify the UI to update
       }
+    } catch (e) {
+      print(e);
     }
-    return moodData;
   }
-  
 }
