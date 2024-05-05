3e4b088c89bf03684e934a42963599225673d4ed
sanchick02 <sanchick02@users.noreply.github.com>
2024-05-05 16:49:16 +0800

    edited forum post providers

--- lib/providers/forum_post_provider.dart
+++ lib/providers/forum_post_provider.dart
@@ -1,24 +1,28 @@
+import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
 import 'package:moodee/models/forum.dart';
 
 class ForumProvider with ChangeNotifier {
-  List<ForumPost> forumPosts = [];
+  List<ForumPost> forumPosts = []; // List to store forum posts
 
   Future<void> fetchUserData() async {
-    if (FirebaseAuth.instance.currentUser != null) {
-      try {
-        // Get the reference to your collection
+    try {
+      FirebaseAuth auth = FirebaseAuth.instance;
+      User? user = auth.currentUser;
+      if (user != null) {
+        // Get the reference to the forum collection
         CollectionReference userForumCollection =
             FirebaseFirestore.instance.collection('new_forum');
 
         // Query the documents within the collection
         QuerySnapshot userPostsSnapshot = await userForumCollection.get();
 
+        forumPosts.clear(); // Empty the list for fresh data
+
         // Extract data from the documents
-        List<ForumPost> fetchedPosts = userPostsSnapshot.docs.map((doc) {
-          return ForumPost(
+        for (var doc in userPostsSnapshot.docs) {
+          ForumPost forumPost = ForumPost(
             uid: doc['uid'],
             pid: doc['pid'],
             userImage: doc['userImage'],
@@ -29,16 +33,16 @@ class ForumProvider with ChangeNotifier {
             likes: doc['likes'],
             mood: doc['mood'],
           );
-        }).toList();
+          forumPosts.add(forumPost);
+        }
 
-        // Update the list of forum posts
-        forumPosts = fetchedPosts;
-
-        // Notify listeners about the change in data
-        notifyListeners();
-      } catch (e) {
-        print(e.toString());
+        print('All forum post data fetched');
+        print(forumPosts[0].userName);
+        print(forumPosts[0].caption);
+        notifyListeners(); // Notify the UI to update
       }
+    } catch (e) {
+      print(e);
     }
   }
 }
